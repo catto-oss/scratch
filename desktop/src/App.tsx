56add926587e8e418288716e25c8a859a492a59b
myferr <contactme.myfer@protonmail.com>
2026-02-06 04:32:21 -0600

    clear boilerplate

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,17 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
-
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
     <main className="container">
       <h1 className="text-red-400">hey</h1>
